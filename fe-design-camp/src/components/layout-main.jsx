7409c3aefe686df190223cb9a83816735cf7cf49
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-03-17 11:47:39 +0700

    feat: landingpage navbar

--- fe-design-camp/src/components/layout-main.jsx
+++ fe-design-camp/src/components/layout-main.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Navbar from "./main/navbar/navbar";
 import Footer from "./main/footer/footer";
+import { plusJakartaSans } from "@/utils/font";
 
 export default function LayoutAdmin({ children }) {
   return (
     <>
       <Navbar />
-      <main className="container mx-auto">{children}</main>
+      <main
+        className={`max-w-screen-[1440px] container py-40 ${plusJakartaSans.className}`}
+      >
+        {children}
+      </main>
       <Footer />
     </>
   );
--- fe-design-camp/src/components/main/navbar/navbar.jsx
+++ fe-design-camp/src/components/main/navbar/navbar.jsx
@@ -1,5 +1,47 @@
 import React from "react";
+import logo from "@/assets/mejeng.webp";
+import Image from "next/image";
+import Link from "next/link";
+import { BiSearch } from "react-icons/bi";
 
 export default function Navbar() {
-  return <div className="container mx-auto">Navbar</div>;
+  return (
+    <nav className=" absolute w-full shadow-md">
+      <div className="container flex h-20 items-center justify-between">
+        <div className="flex items-center justify-center gap-12 font-medium">
+          <Image src={logo} alt="logo" height={30} />
+          <Link href="/inspiration" className="hover:text-primary">
+            Inspiration
+          </Link>
+          <Link href="/discover" className="hover:text-primary">
+            Discover
+          </Link>
+        </div>
+        <div className="flex items-center justify-center gap-11">
+          <div className="flex h-12 w-96 items-center justify-center rounded-full bg-[#F0F1F2] px-5 text-[#9F9F9F]">
+            <input
+              type="text"
+              placeholder="Cari blog dan artikel disini ..."
+              className="w-full bg-transparent focus:outline-none"
+            />
+            <BiSearch size={20} />
+          </div>
+          <div className="flex items-center justify-center gap-3 ">
+            <Link
+              href={"/login"}
+              className="rounded-[15px] py-[14px] px-8 font-bold text-primary transition-all hover:bg-primary hover:text-white hover:shadow-primary/40 hover:drop-shadow-lg"
+            >
+              Log In
+            </Link>
+            <Link
+              href={"/register"}
+              className="rounded-[15px] bg-primary py-[14px] px-8 font-bold text-white"
+            >
+              Sign Up
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
