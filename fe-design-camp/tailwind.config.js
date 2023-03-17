cb3514899feb4b5b7fa2332c78fff1b40e217d06
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-03-17 11:46:05 +0700

    feat: custom config style

--- fe-design-camp/tailwind.config.js
+++ fe-design-camp/tailwind.config.js
@@ -9,7 +9,29 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#213CCC",
+        secondary: "#9900CC",
+      },
+    },
+    container: {
+      center: true,
+      DEFAULT: "1rem",
+      sm: "2rem",
+      md: "3rem",
+      lg: "4rem",
+      xl: "5rem",
+      "2xl": "6rem",
+    },
+    screens: {
+      xs: "320px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1440px",
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
