de686233a58b3ae5b378191c701414e4fbb04ea9
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-03-20 12:51:20 +0700

    feat: add remotePatterns

--- fe-design-camp/next.config.js
+++ fe-design-camp/next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ui-avatars.com",
+        port: "",
+        pathname: "/api/**",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+        pathname: "/seed/**",
+      },
+    ],
+  },
+};
