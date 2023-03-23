11175f162b9b8dabda656ddbd98f186852c28af6
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-03-23 20:15:35 +0700

    perf: add a head component to the application structure

--- fe-design-camp/src/pages/_document.js
+++ fe-design-camp/src/pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Mejeng</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/mejeng-icon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
