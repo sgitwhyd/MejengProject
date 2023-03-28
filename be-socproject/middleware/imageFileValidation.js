7788f165260c79bde5a923add82ff4b4d43574f1
sgitwhyd <sigitwahyu.mutu@gmail.com>
2023-03-28 09:18:54 +0700

    feat: create middleware to handle edit profile image

--- be-socproject/middleware/imageFileValidation.js
+++ be-socproject/middleware/imageFileValidation.js
@@ -74,6 +74,57 @@ const fileFilter = async (req, res, next) => {
 	}
 };
 
+const userProfileImageFilter = async (req, res, next) => {
+	const user_image = req.file;
+	console.log(user_image);
+	try {
+		// CHECK IF NO IMAGE FILE IS UPLOAD
+		if (!req.file) {
+			return res.status(400).json({
+				status: false,
+				error: 'No Image upload',
+			});
+		}
+
+		// CHECK IMAGE FILE VALIDATE
+
+		if (
+			!user_image.mimetype.includes('jpeg') &&
+			!user_image.mimetype.includes('png') &&
+			!user_image.mimetype.includes('jpg')
+		) {
+			fs.unlinkSync(user_image.path);
+			return res.status(400).json({
+				status: false,
+				error: 'Invalid file type. Only JPEG, PNG, JPG are allowed',
+			});
+		}
+
+		// CHECK IMAGE SIZE
+		if (user_image.size > 1024 * 1024) {
+			fs.unlinkSync(user_image.path);
+			return res.status(400).json({
+				status: false,
+				error: 'file too large. Max 1 MB',
+			});
+		} else {
+			next();
+		}
+	} catch (error) {
+		fs.unlinkSync(user_image.path);
+		return res.status(400).json({
+			status: false,
+			error: 'Image Validate Error. Please Contact Developers',
+			msg: error,
+		});
+	}
+};
+
+const userProfileUploadHandler = multer({
+	storage: storage,
+	limits: 1024 * 1024,
+}).single('user_image');
+
 const uploadHandler = multer({
 	storage: storage,
 	limits: 1024 * 1024,
@@ -85,4 +136,6 @@ const uploadHandler = multer({
 module.exports = {
 	uploadHandler,
 	fileFilter,
+	userProfileImageFilter,
+	userProfileUploadHandler,
 };
