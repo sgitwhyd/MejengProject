993004ab23f053f637e2929af1a2855d8614f41f
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-04-10 23:24:36 +0700

    feat: add modal ban-creator & renew table column

--- fe-design-camp/src/pages/admin/admin-profile-creator.jsx
+++ fe-design-camp/src/pages/admin/admin-profile-creator.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import { BsSearch } from "react-icons/bs";
+import { MdReportProblem } from "react-icons/md";
 
 export default function AdminProfileCreator() {
   const [searchText, setSearchText] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalData, setModalData] = useState({});
 
   const datas = [
     {
@@ -12,6 +15,8 @@ export default function AdminProfileCreator() {
       postProject: "43 Project",
       totalView: "2999",
       totalLike: "200",
+      status: true,
+      totalReport: 20,
     },
     {
       name: "Nicko Fajar",
@@ -20,6 +25,8 @@ export default function AdminProfileCreator() {
       postProject: "1 Project",
       totalView: "100",
       totalLike: "10",
+      status: false,
+      totalReport: 90,
     },
     {
       name: "Rizky Fajar",
@@ -28,8 +35,26 @@ export default function AdminProfileCreator() {
       postProject: "2 Project",
       totalView: "200",
       totalLike: "20",
+      status: true,
+      totalReport: 0,
     },
   ];
+
+  const tableHeader = [
+    "Creator Name",
+    "Region & Country",
+    "Post Project",
+    "Total View",
+    "Total Like",
+    "Status",
+    "Total Report",
+    "Action",
+  ];
+
+  const handleModal = (data) => {
+    setIsModalOpen(true);
+    setModalData(data);
+  };
 
   const handleSearch = (e) => {
     setSearchText(e.target.value);
@@ -42,7 +67,11 @@ export default function AdminProfileCreator() {
   return (
     <>
       <header className="text-xl font-bold">Profile Creator</header>
-      <div className="mt-6 mb-2 flex items-center justify-end">
+      <div
+        className={`mt-6 mb-2 flex items-center justify-end ${
+          isModalOpen && "blur-sm"
+        }`}
+      >
         <div className="relative w-full max-w-xs">
           <input
             type="text"
@@ -56,25 +85,25 @@ export default function AdminProfileCreator() {
           </div>
         </div>
       </div>
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <div
+        className={`relative overflow-x-auto shadow-md sm:rounded-lg ${
+          isModalOpen && "blur-sm"
+        }`}
+      >
         <table className="w-full text-left text-sm text-gray-500 ">
           <thead className="bg-gray-50 text-xs uppercase text-gray-700 ">
             <tr>
-              <th scope="col" className="px-6 py-3">
-                Creator Name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Region &amp; Country
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Post Project
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Total View
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Total Like
-              </th>
+              {tableHeader.map((header) => {
+                return (
+                  <th
+                    key={header}
+                    scope="col"
+                    className="whitespace-nowrap px-6 py-3"
+                  >
+                    {header}
+                  </th>
+                );
+              })}
             </tr>
           </thead>
           <tbody>
@@ -93,6 +122,23 @@ export default function AdminProfileCreator() {
                   <td className="px-6 py-4">{data.postProject}</td>
                   <td className="px-6 py-4">{data.totalView}</td>
                   <td className="px-6 py-4">{data.totalLike}</td>
+                  <td className="px-6 py-4">
+                    {data.status ? (
+                      <button className="btn-success btn-xs btn">Active</button>
+                    ) : (
+                      <button className="btn-error btn-xs btn">Disable</button>
+                    )}
+                  </td>
+                  <td className="px-6 py-4">{data.totalReport}</td>
+                  <td className="px-6 py-4">
+                    <button
+                      className="btn-error btn-sm btn gap-2"
+                      onClick={() => handleModal(data)}
+                    >
+                      <MdReportProblem />
+                      Ban Creator
+                    </button>
+                  </td>
                 </tr>
               );
             })}
@@ -106,6 +152,31 @@ export default function AdminProfileCreator() {
           </tbody>
         </table>
       </div>
+      {isModalOpen && (
+        <div className="absolute inset-0 z-[99] mx-auto my-auto  h-[300px] w-[478px] border bg-white shadow-lg drop-shadow-xl">
+          <div className="relative flex h-full w-full flex-col items-center justify-center">
+            <label
+              className="btn-sm btn-circle btn absolute right-2 top-2"
+              onClick={() => setIsModalOpen(false)}
+            >
+              ✕
+            </label>
+            <h1 className="text-lg font-bold">
+              Are you sure to ban this creator?
+            </h1>
+            <p>Creator name : {modalData.name}</p>
+            <div className="flex gap-4">
+              <button
+                className="btn-error btn-sm btn text-white"
+                onClick={() => setIsModalOpen(false)}
+              >
+                Cancel
+              </button>
+              <button className="btn-success btn-sm btn text-white">Yes</button>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
