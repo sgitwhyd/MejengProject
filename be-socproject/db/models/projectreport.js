87cb9ad25ccdbf907361410d8239901a32b1134a
sgitwhyd <sigitwahyu.mutu@gmail.com>
2023-04-11 22:11:11 +0700

    feat: adding association project report

--- be-socproject/db/models/projectreport.js
+++ be-socproject/db/models/projectreport.js
@@ -9,6 +9,10 @@ module.exports = (sequelize, DataTypes) => {
 		 */
 		static associate(models) {
 			// define association here
+			this.belongsTo(models.Project, {
+				foreignKey: 'ProjectId',
+				as: 'project',
+			});
 		}
 	}
 	ProjectReport.init(
