6b6a09c82e2652034771bddba89d8e0f6f773240
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-04-28 16:43:40 +0700

    fix: image details resolution & layout

--- fe-design-camp/src/pages/project/detail-project/[slug]/index.jsx
+++ fe-design-camp/src/pages/project/detail-project/[slug]/index.jsx
@@ -30,6 +30,7 @@ export default function ProjectDetails() {
   const slug = router.query.slug;
 
   const [isChoise, setIsChoise] = useState(false);
+  const [isLoading, setLoading] = useState(true);
 
   const { projectDetail, projectByUser, projectByCategory, loading } =
     useSelector(selectProject);
@@ -83,11 +84,11 @@ export default function ProjectDetails() {
         />
       </Head>
       <section>
-        <h1 className="pb-7 text-3xl font-bold uppercase text-primary">
+        <h1 className="text-3xl font-bold uppercase text-primary">
           {projectDetail?.title}
         </h1>
 
-        <div className="flex items-center justify-start gap-2 pb-3">
+        <div className="flex items-center justify-start gap-2 pt-3 pb-5">
           <img
             width={32}
             height={32}
@@ -107,17 +108,22 @@ export default function ProjectDetails() {
           </p>
         </div>
 
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center px-16">
           <Image
             src={
               projectDetail?.thumbnail_project_image?.includes("lorem")
                 ? projectDetail?.thumbnail_project_image
                 : `${process.env.NEXT_PUBLIC_BE_BASE_URL}/${projectDetail?.thumbnail_project_image}`
             }
-            alt="Uploaded file"
-            width={500}
-            height={500}
-            className="h-full w-full rounded-xl object-cover"
+            alt={projectDetail?.title}
+            width={1200}
+            height={1200}
+            className={`h-full w-full rounded-xl object-cover ${
+              isLoading
+                ? "scale-100 blur-md grayscale"
+                : "scale-100 blur-0 grayscale-0"
+            })`}
+            onLoadingComplete={() => setLoading(false)}
           />
           <div className="px-12 py-5 text-xl ">
             <div
@@ -136,69 +142,73 @@ export default function ProjectDetails() {
                 }
                 alt="Uploaded file"
                 key={index}
-                width={500}
-                height={500}
-                className="h-full w-full rounded-xl object-cover"
+                width={1200}
+                height={1200}
+                className={`h-full w-full rounded-xl object-cover ${
+                  isLoading
+                    ? "scale-100 blur-md grayscale"
+                    : "scale-100 blur-0 grayscale-0"
+                })`}
+                onLoadingComplete={() => setLoading(false)}
               />
             ))}
+          </div>
+        </div>
 
-            <div className="flex w-full flex-col items-center justify-center gap-8 bg-[#122341] p-16 text-white">
-              <h3 className="text-2xl font-semibold capitalize">
-                {projectDetail?.title}
-              </h3>
-              {projectDetail?.url !== "" && (
-                <p className="text-sm font-medium">
-                  If you want to explore a project with all its intricate
-                  details,{" "}
-                  <Link
-                    href={`${projectDetail?.url}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-white hover:text-white/80"
-                  >
-                    click here.
-                  </Link>
-                </p>
+        <div className="my-7 flex w-full flex-col items-center justify-center gap-8 bg-[#122341] p-16 text-white">
+          <h3 className="text-2xl font-semibold capitalize">
+            {projectDetail?.title}
+          </h3>
+          {projectDetail?.url !== "" && (
+            <p className="text-sm font-medium">
+              If you want to explore a project with all its intricate details,{" "}
+              <Link
+                href={`${projectDetail?.url}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white hover:text-white/80"
+              >
+                click here.
+              </Link>
+            </p>
+          )}
+          <div className="flex gap-8">
+            <button
+              className="flex items-center justify-center gap-2 rounded-xl bg-primary px-4 py-3 text-sm font-semibold text-white transition-all duration-300 hover:bg-primary/80"
+              onClick={handleOnUserLike}
+            >
+              {userProjectsLiked
+                ?.map((item) => item.ProjectId)
+                .includes(projectDetail?.id) ? (
+                <>
+                  <AiFillDislike size={18} />
+                  Unppreciate
+                </>
+              ) : (
+                <>
+                  <AiFillLike size={18} />
+                  Appreciate
+                </>
               )}
-              <div className="flex gap-8">
-                <button
-                  className="flex items-center justify-center gap-2 rounded-xl bg-primary px-4 py-3 text-sm font-semibold text-white transition-all duration-300 hover:bg-primary/80"
-                  onClick={handleOnUserLike}
-                >
-                  {userProjectsLiked
-                    ?.map((item) => item.ProjectId)
-                    .includes(projectDetail?.id) ? (
-                    <>
-                      <AiFillDislike size={18} />
-                      Unppreciate
-                    </>
-                  ) : (
-                    <>
-                      <AiFillLike size={18} />
-                      Appreciate
-                    </>
-                  )}
-                </button>
-                <div className="flex gap-8">
-                  <div className="flex items-center justify-center gap-1 text-sm text-white">
-                    <AiFillHeart className="h-5 w-5 transition-all duration-300 hover:text-gray-300" />
-                    <p>{projectDetail?.total_likes}</p>
-                  </div>
-                  <div className="flex items-center justify-center gap-1 text-sm text-white">
-                    <AiFillEye className="h-5 w-5 transition-all duration-300 hover:text-gray-300" />
-                    <p>{projectDetail?.total_views}</p>
-                  </div>
-                </div>
+            </button>
+            <div className="flex gap-8">
+              <div className="flex items-center justify-center gap-1 text-sm text-white">
+                <AiFillHeart className="h-5 w-5 transition-all duration-300 hover:text-gray-300" />
+                <p>{projectDetail?.total_likes}</p>
               </div>
-              <div className="flex items-center justify-center gap-2 font-medium">
-                <p className="pr-5">Published : {projectDetail?.createdAt}</p>
+              <div className="flex items-center justify-center gap-1 text-sm text-white">
+                <AiFillEye className="h-5 w-5 transition-all duration-300 hover:text-gray-300" />
+                <p>{projectDetail?.total_views}</p>
               </div>
             </div>
+          </div>
+          <div className="flex items-center justify-center gap-2 font-medium">
+            <p className="pr-5">Published : {projectDetail?.createdAt}</p>
           </div>
         </div>
 
         {/* Comment Section */}
-        <div className="flex w-full items-start justify-center gap-4 pt-7">
+        <div className="flex w-full items-start justify-center gap-4">
           {/* Comment Field */}
           <div className="w-full border p-10">
             {!login ? (
@@ -235,7 +245,7 @@ export default function ProjectDetails() {
             <div className="flex w-full flex-col items-start justify-center gap-4 border p-4">
               <h1 className="text-sm font-bold text-[#656470]">OWNER</h1>
               <div className="flex items-center justify-center gap-2">
-                <img
+                <Image
                   width={50}
                   height={50}
                   src={
@@ -305,7 +315,7 @@ export default function ProjectDetails() {
                 <h1 className="font-bold text-[#656470]">TOOLS</h1>
                 {projectDetail?.tools?.map((tool, index) => (
                   <div key={index} className="flex items-center gap-[10px]">
-                    <img
+                    <Image
                       src={
                         tool.icon.includes("lorem")
                           ? tool.icon
