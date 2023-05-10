bba86cbd13f4c7bade59b112cabe84e049c14fc6
sgitwhyd <sigitwahyu.mutu@gmail.com>
2023-05-10 22:17:08 +0700

    fix: remove unused variable & adding count of project report

--- fe-design-camp/src/pages/admin/admin-ban-project.jsx
+++ fe-design-camp/src/pages/admin/admin-ban-project.jsx
@@ -8,12 +8,9 @@ import { fetchReportedProjects } from "@/store/admin/admin.action";
 
 import { SuccessToast, ErrorToast } from "@/components/toast/alert-taost";
 import Table from "@/components/table";
+import { selectReportCategories } from "@/store/report/report.selector";
 
 export default function AdminBanProject() {
-	let ammountInappropriate = 0;
-	let ammountSensitive = 0;
-	let ammountPlagiarism = 0;
-
 	const dispatch = useDispatch();
 	const [isModalBanOpen, setIsModalBanOpen] = useState(false);
 	const [isModalDetailReportOpen, setIsModalDetailReportOpen] = useState(false);
@@ -21,6 +18,7 @@ export default function AdminBanProject() {
 	const [modalData, setModalData] = useState({});
 
 	const { reportedProjects, loading } = useSelector(selectAdmin);
+	const { reportCategories } = useSelector(selectReportCategories);
 
 	const handleBanProject = async (id) => {
 		await dispatch(bannProject(id)).then((res) => {
@@ -80,18 +78,27 @@ export default function AdminBanProject() {
 				title: data.title,
 				creator: data.user.name,
 				reportReason: (
-					<div
-						className='btn btn-warning btn-sm capitalize text-white'
-						onClick={() => {
-							setIsModalDetailReportOpen(!isModalDetailReportOpen);
-							setModalData(data);
-						}}>
-						See Detail
+					<div className='indicator'>
+						<span className='badge-primary badge indicator-item'>
+							{data.projectReportCategories.length > 100 ? (
+								<span className='text-white'>100+</span>
+							) : (
+								data.projectReportCategories.length
+							)}
+						</span>
+						<div
+							className='btn-warning btn-sm btn capitalize text-white'
+							onClick={() => {
+								setIsModalDetailReportOpen(!isModalDetailReportOpen);
+								setModalData(data);
+							}}>
+							See Detail
+						</div>
 					</div>
 				),
 				action: (
 					<button
-						className={`btn btn-sm gap-2 capitalize text-white ${
+						className={`btn-sm btn gap-2 capitalize text-white ${
 							data.is_active ? "btn-error " : "btn-disabled bg-black"
 						}`}
 						onClick={() => {
@@ -139,12 +146,12 @@ export default function AdminBanProject() {
 						<p className='my-5'>Creator Name : {modalData.user.name}</p>
 						<div className='mt-3 flex gap-4'>
 							<button
-								className='btn btn-error btn-sm text-white'
+								className='btn-error btn-sm btn text-white'
 								onClick={() => setIsModalBanOpen(false)}>
 								Cancel
 							</button>
 							<button
-								className={`btn btn-success btn-sm text-white ${
+								className={`btn-success btn-sm btn text-white ${
 									loading ? "loading" : ""
 								}`}
 								onClick={() => {
@@ -162,59 +169,39 @@ export default function AdminBanProject() {
 				<div className='absolute inset-0 z-[99] mx-auto my-auto  h-fit w-[478px] rounded-2xl border  bg-white p-10 shadow-lg drop-shadow-xl'>
 					<div className='relative flex h-full w-full flex-col items-center justify-center'>
 						<h1 className='text-center text-lg font-bold'>Detail</h1>
-						<p className='my-5 w-full'>
-							{modalData.projectReportCategories.map((report) => {
-								if (report.name === "Inappropiate") {
-									ammountInappropriate += 1;
-								} else if (report.name === "Plagiarism") {
-									ammountPlagiarism += 1;
-								} else if (report.name === "Sensitive") {
-									ammountSensitive += 1;
-								}
-							})}
+						<div className='my-5 w-full'>
 							<table className='w-full'>
-								<tr>
-									<td>Kategori</td>
-									<td>Jumlah Report</td>
-								</tr>
-								<tr>
-									<td>Inappropriate</td>
-									<td>{ammountInappropriate}</td>
-								</tr>
-								<tr>
-									<td>Plagiarism</td>
-									<td>{ammountPlagiarism}</td>
-								</tr>
-								<tr>
-									<td>Sensitive</td>
-									<td>{ammountSensitive}</td>
-								</tr>
+								<thead>
+									<tr>
+										<th>Kategori</th>
+										<th>Jumlah Report</th>
+									</tr>
+								</thead>
+								<tbody className='text-center'>
+									{reportCategories
+										.filter((category) =>
+											modalData.projectReportCategories.some(
+												(report) => report.name === category.name
+											)
+										)
+										.map((category) => (
+											<tr key={category.id}>
+												<td>{category.name}</td>
+												<td>
+													{
+														modalData.projectReportCategories.filter(
+															(report) => report.name === category.name
+														).length
+													}
+												</td>
+											</tr>
+										))}
+								</tbody>
 							</table>
-							<h4 className='my-5 text-center'>
-								Others{" "}
-								{modalData.projectReportCategories.length -
-									(ammountInappropriate + ammountPlagiarism + ammountSensitive)}
-							</h4>
-							<div
-								className={`h-[300px] overflow-y-auto ${
-									modalData.projectReportCategories.length -
-										(ammountInappropriate +
-											ammountPlagiarism +
-											ammountSensitive) <
-									10
-										? "h-fit"
-										: "h-[200px]"
-								}`}>
-								{modalData.projectReportCategories.map((report, index) => (
-									<ol type='1' key={index}>
-										{report.body && <li>{report.body}</li>}
-									</ol>
-								))}
-							</div>
-						</p>
+						</div>
 						<div className='mt-3 flex gap-4'>
 							<button
-								className='btn btn-error btn-sm text-white'
+								className='btn-error btn-sm btn text-white'
 								onClick={() =>
 									setIsModalDetailReportOpen(!isModalDetailReportOpen)
 								}>
